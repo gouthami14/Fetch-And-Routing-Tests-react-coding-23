cdf9665fa357e2f37b8995d4f805c4752602a097
gouthami14 <92106741+gouthami14@users.noreply.github.com>
2023-07-15 10:47:31 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 import './index.css'
-import UserInfo from '../UserInfo'
 import BlogList from '../BlogList'
+import UserInfo from '../UserInfo'
 
 const Home = () => (
   <div className="home-container">
